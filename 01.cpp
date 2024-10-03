233984bcad9a506a34a02991a785fb12a924d66d
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2024-10-03 08:25:07 +0330

    Create 01.cpp

---
+++ 01.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+float main()
+{
+    float x;
+    float y;
+    float z;
+    
+    cout << "x:";
+    cin >> x;
+    cout << "y:";
+    cin >> y;
+    cout << "z":'
+    cin >> z;
+    float sum = x + y + z;
+    float avrage = sum / 3;
+    cout << avrage;
+}
